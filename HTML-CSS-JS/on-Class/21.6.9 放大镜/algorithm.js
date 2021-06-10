04e832d740883b1891250287b3c480b234830d9a
Jimifa <jimmyhuang784@gmail.com>
2021-06-11 01:19:39 +0800

    update

--- HTML-CSS-JS/on-Class/21.6.9 放大镜/algorithm.js
+++ HTML-CSS-JS/on-Class/21.6.9 放大镜/algorithm.js
@@ -1,20 +1,4 @@
 window.onload = function(){
-//     document.addEventListener('mousemove',function (e) {
-//         //mousemove 只要鼠标一移动，就会触发事件
-//         //获取鼠标最新的坐标
-//         console.log(e.pageY);
-//         console.log(e.pageX);
-//     })
-
-    // document.querySelector(".window").addEventListener("mouseenter", enter);
-    // document.querySelector(".window").addEventListener("mousemove", move);
-    // document.querySelector(".window").addEventListener("mouseleave", leave);
-    // var window = document.getElementById("small");
-    // var smallH = window.offsetHeight; //小窗高度
-    // var smallW = window.offsetWidth; //小窗宽度
-    // var bigS = document.body.clientHeight;
-    // var bigH = document.body.clientWidth;
-    // console.log(smallH,smallW,bigS,bigH);
 
     var pos = document.getElementById("small");
 
