68bdba1206d57cea6829e680654ef620df302109
Jkwang <1923546727@qq.com>
2021-06-14 17:09:14 +0800

    tab

--- HTML-CSS-JS/on-Class/21.6.11 tab页面/tab.js
+++ HTML-CSS-JS/on-Class/21.6.11 tab页面/tab.js
@@ -1,22 +1,20 @@
 window.onload = function(){
 
     var title = document.getElementsByClassName("cate");
-    var content = document.getElementsByClassName("content");
 
-
-    title[0].onclick = function click(n){
+    title[0].onclick = function click(){
         aftTap(0);
     }
 
-    title[1].onclick = function click(n){
+    title[1].onclick = function click(){
         aftTap(1);
     }
 
-    title[2].onclick = function click(n){
+    title[2].onclick = function click(){        
         aftTap(2);
     }
 
-    title[3].onclick = function click(n){
+    title[3].onclick = function click(){       
         aftTap(3);
     }
 
@@ -24,13 +22,29 @@ window.onload = function(){
         title[i].num = i;
     }
 
-    var write = "display: block;";
-
     function aftTap(num){
         console.log(title,num);
-        var pp=document.querySelector(".content:nth-of-type("+num+")");
-        pp.style.display= "block";
 
+        //display:block
+        nth = num + 1;
+        var show = document.querySelector(".content:nth-of-type(" + nth + ")");
+        show.style.display= "block";
+        // var bg = document.querySelector(".content:target:nth-of-type(" + nth + ")");
+        // bg.style.background = "grey";
+
+        //clean 
+        for(hid = 0; hid < title.length; hid++){
+            if(hid != num){            
+                var hidth = hid + 1;
+                var hide = document.querySelector(".content:nth-of-type(" + hidth + ")");
+                hide.style.display= "none";
+            } 
+
+        }
+    
     }
+
+    
+
 }
 
