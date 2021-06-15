bbc290d53a3d0d47ac0ade2eeb794ec9671f289a
Jimifa <jimmyhuang784@gmail.com>
2021-06-16 01:48:27 +0800

    almost done

--- HTML-CSS-JS/on-Class/21.6.14 百叶窗/folder.js
+++ HTML-CSS-JS/on-Class/21.6.14 百叶窗/folder.js
@@ -1,53 +1,49 @@
 window.onload = function(){
 
-    /* tab-cate-content */
-    var cate = document.getElementsByClassName("cate");
+    /* tab-title~content */
+    var tab = document.getElementsByClassName("tab");
+    var title = document.getElementsByClassName("title");
+    var content = document.getElementsByClassName("content");
 
-    cate[0].onclick = function click(){
+    // console.log(tab);
+    // console.log(title);
+    // console.log(content);
+
+    title[0].onclick = function click(){
+        dropBack(0);
+    };
+    
+    title[1].onclick = function click(){
         dropBack(1);
     };
-    
-    cate[1].onclick = function click(){
 
-    };
-
-    cate[2].onclick = function click(){
-
+    title[2].onclick = function click(){
+        dropBack(2);
     };
     
-    cate[3].onclick = function click(){
-
+    title[3].onclick = function click(){
+        dropBack(3);
     };
 
-    for(var i = 0; i < cate.length; i++){
-        cate[i].num = i;
+    for(var i = 0; i < title.length; i++){
+        title[i].num = i;
     }
+
 
     function dropBack(num){
-        nth = num + 1;
-        var cateDrop = document.querySelector(".content:nth-of-type(" + nth + ")");
-        cateDrop.style.display = "block";
-        cateDrop.style.transition = "translateX(-10)";
+        //drop
+        nth = (num + 1) * 2;
+        var cttDrop = document.querySelector(".content:nth-of-type(" + nth + ")");
+        // console.log(num,cttDrop);
+        cttDrop.style.display = "block";
+
+        //tap-other-back
+        for(hid = 0; hid < title.length; hid++){
+            if(hid != num){            
+                var hidth = (hid + 1) * 2;
+                var hide = document.querySelector(".content:nth-of-type(" + hidth + ")");
+                hide.style.display= "none";          
+            }
+        }  
     }
-
-    // function aftTap(){
-    //     console.log(title,num);
-
-    //     //display:block
-    //     
-    //     
-    //     show.style.display= "block";
-    //     // var bg = document.querySelector(".content:target:nth-of-type(" + nth + ")");
-    //     // bg.style.background = "grey";
-
-    //     //clean 
-    //     for(hid = 0; hid < title.length; hid++){
-    //         if(hid != num){            
-    //             var hidth = hid + 1;
-    //             var hide = document.querySelector(".content:nth-of-type(" + hidth + ")");
-    //             hide.style.display= "none";
-    //         } 
-
-    //     }
-
 }
