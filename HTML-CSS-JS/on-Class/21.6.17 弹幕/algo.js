211141c45e2a40a23d9dbe3f40f84669c7f00c16
Jimifa <jimmyhuang784@gmail.com>
2021-06-24 00:15:30 +0800

    。

--- HTML-CSS-JS/on-Class/21.6.17 弹幕/algo.js
+++ HTML-CSS-JS/on-Class/21.6.17 弹幕/algo.js
@@ -48,7 +48,7 @@ window.onload = function(){
 
             function makeSpan(){
                 var span = document.createElement('span');
-                span.appendChild(textShoot);
+                span.setAttribute                
                 console.log(span);
             }
             makeSpan();
