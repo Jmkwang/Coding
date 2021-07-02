72b4f2e0b6656cbd006c05d4d0b822874b2afeef
Jimifa <jimmyhuang784@gmail.com>
2021-07-02 16:13:41 +0800

    finished two

--- HTML-CSS-JS/on-Class/21.7.1 轮播【集合】/lunbo.js
+++ HTML-CSS-JS/on-Class/21.7.1 轮播【集合】/lunbo.js
@@ -1,18 +1,75 @@
 window.onload=function(){
+
+    var pics_pool =["./pics/1.jpg","./pics/2.jpg","./pics/3.jpg","./pics/4.jpg"];
+
     //time
-    var time_pool=["./pics/1.jpg","./pics/2.jpg","./pics/3.jpg","./pics/4.jpg"]
-    var pics = document.getElementById("time_pics");
+    var time_pics = document.getElementById("time_pics");
     var cnt1 = 0;
     
     setInterval(() => {
-        var timePic = time_pool[cnt1];
-        var realSrc = pics.setAttribute("src", timePic); 
+        var tPic = pics_pool[cnt1];
+        var tSrc = time_pics.setAttribute("src", tPic); 
         cnt1 ++;
-        if(cnt1 == time_pool.length){
+        if(cnt1 == pics_pool.length){
             cnt1 = 0;
         }
     }, 2000);
     
+
     //click
+    var left = document.getElementsByClassName("click_left")[0];
+    var right = document.getElementsByClassName("click_right")[0];
+    var click_pics = document.getElementById("click_pics");
+
+    var current_count = 0;
+
+    console.log(left);
+    console.log(right);
+    console.log(click_pics);
+
+    left.onclick = function(){
+        console.log("left");
+        if(current_count == 0){
+            current_count = 3;
+        }else{
+            current_count -= 1;
+        }
+        clickNchange(current_count);
+    }
+
+    right.onclick = function(){
+        console.log("right");
+        if(current_count == 3){
+            current_count = 0;
+        }else{
+            current_count += 1;
+        }
+        clickNchange(current_count);
+    }
+
+    function clickNchange(cnum){
+        var cPic = pics_pool[cnum];
+        var cSrc = click_pics.setAttribute("src", cPic);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
