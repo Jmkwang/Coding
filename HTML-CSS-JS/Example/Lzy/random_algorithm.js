5f0bbfe4b785201eb8bb065f223176ebdb582ead
Jimifa <jimmyhuang784@gmail.com>
2021-07-09 16:34:08 +0800

    Finished.

--- HTML-CSS-JS/Example/Lzy/random_algorithm.js
+++ HTML-CSS-JS/Example/Lzy/random_algorithm.js
@@ -1,17 +1,42 @@
-var arr = [1,2,3,4,5];
-var arrNew = [];
-
-//存放用过的随机数
-var jishu= [];
-
-for(var i = 0; i < arr.length; i++){
-    do {
-        var random = Math.floor(Math.random() * 5);
-    } while (random == i || jishu.indexOf(random) !== -1);  //随机出的索引不等于原数组的索引
-
-    arrNew[random] = arr[i];
-    jishu.push(random);
+//min ≤ r ≤ max
+function RandomNumBoth(Min,Max){
+    var Range = Max - Min;
+    var Rand = Math.random();
+    var num = Min + Math.round(Rand * Range); //四舍五入
+    return num;
 }
 
-console.log(arrNew);
+//min ≤ r < max
+function RandomNum(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    var num = Min + Math.floor(Rand * Range); //舍去
+    return num;
+}
 
+//min < r ≤ max
+function RandomNum(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    if(Math.round(Rand * Range)==0){       
+      return Min + 1;
+    }
+    var num = Min + Math.round(Rand * Range);
+    return num;
+}
+
+//min < r < max 
+function RandomNum(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    if(Math.round(Rand * Range)==0){
+      return Min + 1;
+    }else if(Math.round(Rand * Max)==Max)
+    {
+      index++;
+      return Max - 1;
+    }else{
+      var num = Min + Math.round(Rand * Range) - 1;
+      return num;
+    }
+}
